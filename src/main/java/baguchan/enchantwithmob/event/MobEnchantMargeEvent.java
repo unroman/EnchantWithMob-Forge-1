f381339daeda80ee0a6a2b8a0a4816722a03d95f
baguchan <shutux500@gmail.com>
2024-01-22 22:07:12 +0900

    support slime split and apply the mob enchant

--- src/main/java/baguchan/enchantwithmob/event/MobEnchantMargeEvent.java
+++ src/main/java/baguchan/enchantwithmob/event/MobEnchantMargeEvent.java
@@ -4,9 +4,13 @@ import baguchan.enchantwithmob.EnchantWithMob;
 import baguchan.enchantwithmob.api.IEnchantCap;
 import baguchan.enchantwithmob.capability.MobEnchantHandler;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.living.LivingConversionEvent;
+import net.neoforged.neoforge.event.entity.living.MobSplitEvent;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = EnchantWithMob.MODID)
 public class MobEnchantMargeEvent {
@@ -21,6 +25,23 @@ public class MobEnchantMargeEvent {
                 if (livingCap.getEnchantCap().hasEnchant()) {
                     for (MobEnchantHandler mobEnchantHandler : livingCap.getEnchantCap().getMobEnchants()) {
                         cap.getEnchantCap().addMobEnchant(outcome, mobEnchantHandler.getMobEnchant(), mobEnchantHandler.getEnchantLevel());
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onEntitySplit(MobSplitEvent event) {
+        LivingEntity livingEntity = event.getParent();
+        List<Mob> children = event.getChildren();
+        for (Mob outcome : children) {
+            if (outcome instanceof IEnchantCap cap) {
+                if (livingEntity instanceof IEnchantCap livingCap) {
+                    if (livingCap.getEnchantCap().hasEnchant()) {
+                        for (MobEnchantHandler mobEnchantHandler : livingCap.getEnchantCap().getMobEnchants()) {
+                            cap.getEnchantCap().addMobEnchant(outcome, mobEnchantHandler.getMobEnchant(), mobEnchantHandler.getEnchantLevel());
+                        }
                     }
                 }
             }
