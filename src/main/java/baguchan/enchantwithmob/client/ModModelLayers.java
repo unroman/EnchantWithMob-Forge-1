dcb1ca94a8edca661779cea0bc0d9438dc06d7ae
baguchan <shutux500@gmail.com>
2024-03-26 16:28:40 +0900

    fix model layer

--- src/main/java/baguchan/enchantwithmob/client/ModModelLayers.java
+++ src/main/java/baguchan/enchantwithmob/client/ModModelLayers.java
@@ -5,7 +5,7 @@ import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.ResourceLocation;
 
 public class ModModelLayers {
-	public static ModelLayerLocation ENCHANTER = new ModelLayerLocation(new ResourceLocation(EnchantWithMob.MODID, "enchanter"), "enchanter");
-	public static ModelLayerLocation ENCHANTER_CLOTHES = new ModelLayerLocation(new ResourceLocation(EnchantWithMob.MODID, "enchanter_clothes"), "enchanter_clothes");
+	public static ModelLayerLocation ENCHANTER = new ModelLayerLocation(new ResourceLocation(EnchantWithMob.MODID, "enchanter"), "main");
+	public static ModelLayerLocation ENCHANTER_CLOTHES = new ModelLayerLocation(new ResourceLocation(EnchantWithMob.MODID, "enchanter_clothes"), "main");
 
 }
